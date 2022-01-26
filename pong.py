d0ab0e6653162f54bc4bed2f8775df6687231313
Kingsley Pan <98239609+Wilbur12456@users.noreply.github.com>
2022-01-26 11:54:07 -0500

    Update pong.py

--- pong.py
+++ pong.py
@@ -91,12 +91,12 @@ while True:
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
-        os.system("afplay bounce.wav&")
+        
     
     elif ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
-        os.system("afplay bounce.wav&")
+       
 
     # Left and right
     if ball.xcor() > 350:
@@ -116,9 +116,9 @@ while True:
     # Paddle and ball collisions
     if ball.xcor() < -340 and ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50:
         ball.dx *= -1 
-        os.system("afplay bounce.wav&")
+        
     
     elif ball.xcor() > 340 and ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50:
         ball.dx *= -1
-        os.system("afplay bounce.wav&")
+       
     
